89acdd806b2d9f3698cce715d7546585d0e2ae97
evelina <evelinayuzych@gmail.com>
2026-07-08 09:35:39 -0400

    Revert "feat: add likes and lightbulbs reaction fields to Article schema"
    This reverts commit a3bdb9fe5e66afaa52f91cdeb325223f1247968a.

--- config/server.js
+++ config/server.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
-  url: 'http://localhost:1338',
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1338),
+  port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
